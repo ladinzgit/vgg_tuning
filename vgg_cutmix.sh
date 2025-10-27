80da8fa7783ce2ba1b3340d435358804d6170420
DinzGIT <ygg33333@gmail.com>
2025-10-27 16:24:34 +0900

    Update SLURM output/error log file naming in vgg_crop.sh

--- vgg_cutmix.sh
+++ vgg_cutmix.sh
@@ -6,8 +6,8 @@
 #SBATCH --mem-per-gpu=30G
 #SBATCH -p batch_ugrad
 #SBATCH -t 1-0
-#SBATCH -o logs/slurm/%x-%A_%a.out
-#SBATCH -e logs/slurm/%x-%A_%a.err
+#SBATCH -o logs/slurm-%A.out
+#SBATCH -e logs/slurm-%A.err
 
 set -euo pipefail
 mkdir -p logs/slurm logs/cutmix5
